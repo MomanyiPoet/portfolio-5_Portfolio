2c9222a651b71e6a7dcde30d2ffcf842ae998644
MomanyiPoet <momanyiturner@gmail.com>
2023-03-29 21:15:10 +0300

    added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Hide Header on Scroll Down Show on Scroll Up
+let lastScrollTop = 0;
+navbar = document.getElementById("navbar")
+window.addEventListener("scroll", function(){
+    let scrollTop = window.pageYOffset || document.doctype.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-4rem";
+    } else {
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+})
+
+// PRELOADER
+let loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
